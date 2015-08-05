b36e89258f3af5a548b3163409534a65d73e781f
Gabriel-Alexandru Badescu <a-gabade@microsoft.com>
2015-08-05 19:49:36 +0300

    fixed prob

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -36,7 +36,7 @@ angular.module('starter.controllers', [])
     //     console.log('events: '+JSON.stringify(data));
     //     }
     //   }
-      );
+    //  )
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
