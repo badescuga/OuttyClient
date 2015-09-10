0e8cd8722f8f048342c074c0b0ef1765771e68be
Gabriel-Alexandru Badescu <badescuga@gmail.com>
2015-09-10 10:46:52 +0300

    work on new group chat

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,230 +1,229 @@
 /* global angular */
 angular.module('starter.controllers', [])
 
-  .controller('LoginCtrl', function($scope, $state, FBFactory, LocalStorage) {
-  //  $scope.chat = Chats.get($stateParams.chatId);
-  //alert('LOGIN CTRL');
- console.log('---  in LoginCTRL');
- //           $state.go('tab.dash'); //temp hack
+  .controller('LoginCtrl', function ($scope, $state, FBFactory, LocalStorage) {
+    //  $scope.chat = Chats.get($stateParams.chatId);
+    //alert('LOGIN CTRL');
+    console.log('---  in LoginCTRL');
+    //           $state.go('tab.dash'); //temp hack
 
-  if (LocalStorage.getFacebookUserData() != null) {
-            $state.go('tab.dash');
-  } else {
-    console.log("user is not logged in -- ");
-  }
+    if (LocalStorage.getFacebookUserData() != null) {
+      $state.go('tab.dash');
+    } else {
+      console.log("user is not logged in -- ");
+    }
 
-  $scope.fbLogin = function () {
-    FBFactory.login(function(error) {
-      if (!error) {
-        console.log("fb login succesful");
-        $state.go('tab.dash');
-      } else {
-        console.log('fb login failed: ' + error);
-      }
-    });
-  };
-})
+    $scope.fbLogin = function () {
+      FBFactory.login(function (error) {
+        if (!error) {
+          console.log("fb login succesful");
+          $state.go('tab.dash');
+        } else {
+          console.log('fb login failed: ' + error);
+        }
+      });
+    };
+  })
 
-  .controller('DashCtrl', function($scope, socketFactory, LocalStorage) {
+  .controller('DashCtrl', function ($scope, socketFactory, LocalStorage) {
 
     //login
     var data = {};
     data.userLoginData = LocalStorage.getFacebookUserData();
     data.userPhotoData = LocalStorage.getFacebookUserPhotoData();
-    console.log('sending to server: '+JSON.stringify(data));
-    socketFactory.login(data, function(response)
-      {
-        console.log('response from server on login: '+  JSON.stringify(response));
-      });
-
-   })
-
-   .controller('ChatDetailCtrl', ['$scope', '$rootScope', '$state',
-  '$stateParams', 'MockService', '$ionicActionSheet',
-  '$ionicPopup', '$ionicScrollDelegate', '$timeout', '$interval',
-  function($scope, $rootScope, $state, $stateParams, MockService,
-    $ionicActionSheet,
-    $ionicPopup, $ionicScrollDelegate, $timeout, $interval) {
-
-    // mock acquiring data via $stateParams
-    $scope.toUser = {
-      _id: '534b8e5aaa5e7afc1b23e69b',
-      pic: 'http://ionicframework.com/img/docs/venkman.jpg',
-      username: 'Venkman'
-    }
-
-    // this could be on $rootScope rather than in $stateParams
-    $scope.user = {
-      _id: '534b8fb2aa5e7afc1b23e69c',
-      pic: 'http://ionicframework.com/img/docs/mcfly.jpg',
-      username: 'Marty'
-    };
-
-    $scope.input = {
-      message: localStorage['userMessage-' + $scope.toUser._id] || ''
-    };
-
-    var messageCheckTimer;
-
-    var viewScroll = $ionicScrollDelegate.$getByHandle('userMessageScroll');
-    var footerBar; // gets set in $ionicView.enter
-    var scroller;
-    var txtInput; // ^^^
-
-    $scope.$on('$ionicView.enter', function() {
-      console.log('UserMessages $ionicView.enter');
-
-      getMessages();
-
-      $timeout(function() {
-        footerBar = document.body.querySelector('#chat-detail .bar-footer');
-        scroller = document.body.querySelector('#chat-detail .scroll-content');
-        txtInput = angular.element(footerBar.querySelector('textarea'));
-      }, 0);
-
-      messageCheckTimer = $interval(function() {
-        // here you could check for new messages if your app doesn't use push notifications or user disabled them
-      }, 20000);
+    console.log('sending to server: ' + JSON.stringify(data));
+    socketFactory.login(data, function (response) {
+      console.log('response from server on login: ' + JSON.stringify(response));
     });
 
-    $scope.$on('$ionicView.leave', function() {
-      console.log('leaving UserMessages view, destroying interval');
-      // Make sure that the interval is destroyed
-      if (angular.isDefined(messageCheckTimer)) {
-        $interval.cancel(messageCheckTimer);
-        messageCheckTimer = undefined;
+  })
+
+  .controller('ChatDetailCtrl', ['$scope', '$rootScope', '$state',
+    '$stateParams', 'MockService', '$ionicActionSheet',
+    '$ionicPopup', '$ionicScrollDelegate', '$timeout', '$interval',
+    function ($scope, $rootScope, $state, $stateParams, MockService,
+      $ionicActionSheet,
+      $ionicPopup, $ionicScrollDelegate, $timeout, $interval) {
+
+      // mock acquiring data via $stateParams
+      $scope.toUser = {
+        _id: '534b8e5aaa5e7afc1b23e69b',
+        pic: 'http://ionicframework.com/img/docs/venkman.jpg',
+        username: 'Venkman'
       }
-    });
 
-    $scope.$on('$ionicView.beforeLeave', function() {
-      if (!$scope.input.message || $scope.input.message === '') {
-        localStorage.removeItem('userMessage-' + $scope.toUser._id);
-      }
-    });
-
-    function getMessages() {
-      // the service is mock but you would probably pass the toUser's GUID here
-      MockService.getUserMessages({
-        toUserId: $scope.toUser._id
-      }).then(function(data) {
-        $scope.doneLoading = true;
-        $scope.messages = data.messages;
-
-        $timeout(function() {
-          viewScroll.scrollBottom();
-        }, 0);
-      });
-    }
-
-    $scope.$watch('input.message', function(newValue, oldValue) {
-      console.log('input.message $watch, newValue ' + newValue);
-      if (!newValue) newValue = '';
-      localStorage['userMessage-' + $scope.toUser._id] = newValue;
-    });
-
-    $scope.sendMessage = function(sendMessageForm) {
-      var message = {
-        toId: $scope.toUser._id,
-        text: $scope.input.message
+      // this could be on $rootScope rather than in $stateParams
+      $scope.user = {
+        _id: '534b8fb2aa5e7afc1b23e69c',
+        pic: 'http://ionicframework.com/img/docs/mcfly.jpg',
+        username: 'Marty'
       };
 
-      // if you do a web service call this will be needed as well as before the viewScroll calls
-      // you can't see the effect of this in the browser it needs to be used on a real device
-      // for some reason the one time blur event is not firing in the browser but does on devices
-      keepKeyboardOpen();
+      $scope.input = {
+        message: localStorage['userMessage-' + $scope.toUser._id] || ''
+      };
 
-      //MockService.sendMessage(message).then(function(data) {
-      $scope.input.message = '';
+      var messageCheckTimer;
 
-      message._id = new Date().getTime(); // :~)
-      message.date = new Date();
-      message.username = $scope.user.username;
-      message.userId = $scope.user._id;
-      message.pic = $scope.user.picture;
+      var viewScroll = $ionicScrollDelegate.$getByHandle('userMessageScroll');
+      var footerBar; // gets set in $ionicView.enter
+      var scroller;
+      var txtInput; // ^^^
 
-      $scope.messages.push(message);
+      $scope.$on('$ionicView.enter', function () {
+        console.log('UserMessages $ionicView.enter');
 
-      $timeout(function() {
-        keepKeyboardOpen();
-        viewScroll.scrollBottom(true);
-      }, 0);
+        getMessages();
 
-      $timeout(function() {
-        $scope.messages.push(MockService.getMockMessage());
-        keepKeyboardOpen();
-        viewScroll.scrollBottom(true);
-      }, 2000);
+        $timeout(function () {
+          footerBar = document.body.querySelector('#chat-detail .bar-footer');
+          scroller = document.body.querySelector('#chat-detail .scroll-content');
+          txtInput = angular.element(footerBar.querySelector('textarea'));
+        }, 0);
 
-      //});
-    };
-
-    // this keeps the keyboard open on a device only after sending a message, it is non obtrusive
-    function keepKeyboardOpen() {
-      console.log('keepKeyboardOpen');
-      txtInput.one('blur', function() {
-        console.log('textarea blur, focus back on it');
-        txtInput[0].focus();
+        messageCheckTimer = $interval(function () {
+          // here you could check for new messages if your app doesn't use push notifications or user disabled them
+        }, 20000);
       });
-    }
 
-    $scope.onMessageHold = function(e, itemIndex, message) {
-      console.log('onMessageHold');
-      console.log('message: ' + JSON.stringify(message, null, 2));
-      $ionicActionSheet.show({
-        buttons: [{
-          text: 'Copy Text'
-        }, {
-          text: 'Delete Message'
-        }],
-        buttonClicked: function(index) {
-          switch (index) {
-            case 0: // Copy Text
-              //cordova.plugins.clipboard.copy(message.text);
-
-              break;
-            case 1: // Delete
-              // no server side secrets here :~)
-              $scope.messages.splice(itemIndex, 1);
-              $timeout(function() {
-                viewScroll.resize();
-              }, 0);
-
-              break;
-          }
-
-          return true;
+      $scope.$on('$ionicView.leave', function () {
+        console.log('leaving UserMessages view, destroying interval');
+        // Make sure that the interval is destroyed
+        if (angular.isDefined(messageCheckTimer)) {
+          $interval.cancel(messageCheckTimer);
+          messageCheckTimer = undefined;
         }
       });
-    };
 
-    // this prob seems weird here but I have reasons for this in my app, secret!
-    $scope.viewProfile = function(msg) {
-      if (msg.userId === $scope.user._id) {
-        // go to your profile
-      } else {
-        // go to other users profile
+      $scope.$on('$ionicView.beforeLeave', function () {
+        if (!$scope.input.message || $scope.input.message === '') {
+          localStorage.removeItem('userMessage-' + $scope.toUser._id);
+        }
+      });
+
+      function getMessages() {
+        // the service is mock but you would probably pass the toUser's GUID here
+        MockService.getUserMessages({
+          toUserId: $scope.toUser._id
+        }).then(function (data) {
+          $scope.doneLoading = true;
+          $scope.messages = data.messages;
+
+          $timeout(function () {
+            viewScroll.scrollBottom();
+          }, 0);
+        });
       }
-    };
 
-    // I emit this event from the monospaced.elastic directive, read line 480
-    $scope.$on('taResize', function(e, ta) {
-      console.log('taResize');
-      if (!ta) return;
+      $scope.$watch('input.message', function (newValue, oldValue) {
+        console.log('input.message $watch, newValue ' + newValue);
+        if (!newValue) newValue = '';
+        localStorage['userMessage-' + $scope.toUser._id] = newValue;
+      });
 
-      var taHeight = ta[0].offsetHeight;
-      console.log('taHeight: ' + taHeight);
+      $scope.sendMessage = function (sendMessageForm) {
+        var message = {
+          toId: $scope.toUser._id,
+          text: $scope.input.message
+        };
 
-      if (!footerBar) return;
+        // if you do a web service call this will be needed as well as before the viewScroll calls
+        // you can't see the effect of this in the browser it needs to be used on a real device
+        // for some reason the one time blur event is not firing in the browser but does on devices
+        keepKeyboardOpen();
 
-      var newFooterHeight = taHeight + 10;
-      newFooterHeight = (newFooterHeight > 44) ? newFooterHeight : 44;
+        //MockService.sendMessage(message).then(function(data) {
+        $scope.input.message = '';
 
-      footerBar.style.height = newFooterHeight + 'px';
-      scroller.style.bottom = newFooterHeight + 'px';
-    });
+        message._id = new Date().getTime(); // :~)
+        message.date = new Date();
+        message.username = $scope.user.username;
+        message.userId = $scope.user._id;
+        message.pic = $scope.user.picture;
 
-}])
-  .controller('ChatsCtrl', function($scope, Chats, FBFactory) {
+        $scope.messages.push(message);
+
+        $timeout(function () {
+          keepKeyboardOpen();
+          viewScroll.scrollBottom(true);
+        }, 0);
+
+        $timeout(function () {
+          $scope.messages.push(MockService.getMockMessage());
+          keepKeyboardOpen();
+          viewScroll.scrollBottom(true);
+        }, 2000);
+
+        //});
+      };
+
+      // this keeps the keyboard open on a device only after sending a message, it is non obtrusive
+      function keepKeyboardOpen() {
+        console.log('keepKeyboardOpen');
+        txtInput.one('blur', function () {
+          console.log('textarea blur, focus back on it');
+          txtInput[0].focus();
+        });
+      }
+
+      $scope.onMessageHold = function (e, itemIndex, message) {
+        console.log('onMessageHold');
+        console.log('message: ' + JSON.stringify(message, null, 2));
+        $ionicActionSheet.show({
+          buttons: [{
+            text: 'Copy Text'
+          }, {
+              text: 'Delete Message'
+            }],
+          buttonClicked: function (index) {
+            switch (index) {
+              case 0: // Copy Text
+                //cordova.plugins.clipboard.copy(message.text);
+
+                break;
+              case 1: // Delete
+                // no server side secrets here :~)
+                $scope.messages.splice(itemIndex, 1);
+                $timeout(function () {
+                  viewScroll.resize();
+                }, 0);
+
+                break;
+            }
+
+            return true;
+          }
+        });
+      };
+
+      // this prob seems weird here but I have reasons for this in my app, secret!
+      $scope.viewProfile = function (msg) {
+        if (msg.userId === $scope.user._id) {
+          // go to your profile
+        } else {
+          // go to other users profile
+        }
+      };
+
+      // I emit this event from the monospaced.elastic directive, read line 480
+      $scope.$on('taResize', function (e, ta) {
+        console.log('taResize');
+        if (!ta) return;
+
+        var taHeight = ta[0].offsetHeight;
+        console.log('taHeight: ' + taHeight);
+
+        if (!footerBar) return;
+
+        var newFooterHeight = taHeight + 10;
+        newFooterHeight = (newFooterHeight > 44) ? newFooterHeight : 44;
+
+        footerBar.style.height = newFooterHeight + 'px';
+        scroller.style.bottom = newFooterHeight + 'px';
+      });
+
+    }])
+  .controller('ChatsCtrl', function ($scope, Chats, FBFactory) {
     // FBFactory.getEvents('10153152163398402',function(err, data)
     //   {
     //     if(err)
@@ -236,31 +235,35 @@ angular.module('starter.controllers', [])
     //   }
     //  )
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function (chat) {
-    Chats.remove(chat);
-  }
-})
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+      Chats.remove(chat);
+    }
+  })
 
 //   .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
 //   $scope.chat = Chats.get($stateParams.chatId);
 // })
 
   .controller('AccountCtrl', function ($scope) {
-  //  alert('daaaaa');
-  $scope.settings = {
-    enableFriends: true
-  };
+    //  alert('daaaaa');
+    $scope.settings = {
+      enableFriends: true
+    };
 
-})
- .controller('NewChatCtrl', function ($scope) {
-   console.log('in new chat ctrl');
+  })
+  .controller('NewChatCtrl', function ($scope) {
+    console.log('in new chat ctrl');
+
+    $scope.createGroup = function () {
+      console.log("!!!!!!!!!!!!!!!!! " + $scope.createGroup.groupName);
+    }
   });
